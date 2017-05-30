639640d839bc55f988e0a5dc6b0652f26a088c09
Bruno Mateus <bruno.mateus@feedzai.com>
2017-05-30 11:57:14 +0100

    Implemented custom behaviour that considers the value of the @type attribute on the request payload, to only validate against specific schemas.

--- src/main/java/com/networknt/schema/OneOfValidator.java
+++ src/main/java/com/networknt/schema/OneOfValidator.java
@@ -47,21 +47,42 @@ public class OneOfValidator extends BaseJsonValidator implements JsonValidator {
 
         int numberOfValidSchema = 0;
         Set<ValidationMessage> errors = new HashSet<>();
-        
+
+        // ------- Custom behaviour that should be migrated when the Custom Validators are supported -------
+        // Please refer to https://github.com/networknt/json-schema-validator/issues/32 for more details.
+
+        // The "@type" value on the request payload.
+        JsonNode nodeType = node.findValue("@type");
+        if(nodeType != null) {
+            // The relation between the "@type" value on the request payload and the schema is through the schema "title" attribute.
+            // On the schemas list, we have to return only the schema that matches with the "@type" value.
+            JsonSchema schema = schemas.stream().filter(s -> getSchemaNodeTitle(s).equals(nodeType.asText())).findAny().orElse(null);
+            // If no match was found, we follow the default behaviour validating against all schemas.
+            if(schema != null) {
+                Set<ValidationMessage> schemaErrors = schema.validate(node, rootNode, at);
+                if (!schemaErrors.isEmpty()) {
+                    errors.addAll(schemaErrors);
+                }
+                return errors;
+            }
+        }
+
+        // ------------------------------------------------------------------------------------------------
+
         for (JsonSchema schema : schemas) {
-        	Set<ValidationMessage> schemaErrors = schema.validate(node, rootNode, at);
+            Set<ValidationMessage> schemaErrors = schema.validate(node, rootNode, at);
             if (schemaErrors.isEmpty()) {
                 numberOfValidSchema++;
                 errors = new HashSet<>();
             }
-            if(numberOfValidSchema == 0){
-        		errors.addAll(schemaErrors);
-        	}
+            if (numberOfValidSchema == 0) {
+                errors.addAll(schemaErrors);
+            }
             if (numberOfValidSchema > 1) {
                 break;
             }
         }
-        
+
         if (numberOfValidSchema > 1) {
         	errors = new HashSet<>();
         	errors.add(buildValidationMessage(at, ""));
@@ -70,4 +91,25 @@ public class OneOfValidator extends BaseJsonValidator implements JsonValidator {
         return errors;
     }
 
+    private String getSchemaNodeTitle(JsonSchema schema) {
+        // oneOf node $ref value.
+        JsonNode schemaNodeRef = schema.getSchemaNode().get("$ref");
+        if(schemaNodeRef == null) {
+            return null;
+        }
+
+        // Node that is referenced by the $ref value on the oneOf node.
+        JsonNode schemaNode = schema.getRefSchemaNode(schemaNodeRef.asText());
+        if(schemaNode == null) {
+            return null;
+        }
+
+        // The title of the node.
+        JsonNode schemaNodeTitle = schemaNode.get("title");
+        if(schemaNodeTitle == null) {
+            return null;
+        }
+
+        return schemaNodeTitle.asText();
+    }
 }
